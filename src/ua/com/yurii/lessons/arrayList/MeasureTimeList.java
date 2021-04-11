44043a556e488a55e3d2ce834d043efd88c5cf9b
YuriiLipchanskiy <YuriiLipchanskiy>
2021-04-11 22:57:10 +0300

    Add file MeasureTimeList

---
+++ src/ua/com/yurii/lessons/arrayList/MeasureTimeList.java
@@ -0,0 +1,32 @@
+package ua.com.yurii.lessons.arrayList;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MeasureTimeList {
+    public static void main(String[] args) {
+
+        List<Integer> linkedList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        measureTime(linkedList);
+        measureTime(arrayList);
+    }
+
+    private static void measureTime(List<Integer> list) {
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000000; i++) {
+            list.add(i);
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+    }
+
+}
+
+
+
