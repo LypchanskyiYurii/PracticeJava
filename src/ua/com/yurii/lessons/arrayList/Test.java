12d9ac122a8aeedc91a8faf2b4dc014fe20e86cf
YuriiLipchanskiy <YuriiLipchanskiy>
2021-04-13 23:09:45 +0300

    Add file MyLinkedList.java and Test.java

---
+++ src/ua/com/yurii/lessons/arrayList/Test.java
@@ -0,0 +1,12 @@
+package ua.com.yurii.lessons.arrayList;
+
+public class Test {
+    public static void main(String[] args) {
+       MyLinkedList myLinkedList = new MyLinkedList();
+       myLinkedList.add(1);
+       myLinkedList.add(2);
+       myLinkedList.add(10);
+
+        System.out.println(myLinkedList);
+    }
+}
