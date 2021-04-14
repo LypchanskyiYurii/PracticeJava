2d05cbd3f323875f020372c34dd7dd36120a61eb
YuriiLipchanskiy <YuriiLipchanskiy>
2021-04-14 22:14:37 +0300

    Add package hashMap

---
+++ src/ua/com/yurii/lessons/hashMap/Test.java
@@ -0,0 +1,26 @@
+package ua.com.yurii.lessons.hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        System.out.println(map);
+
+        System.out.println(map.get(2));
+        System.out.println(map.get(10));
+
+        map.put(1, "One + one");
+        System.out.println(map);
+    }
+}
